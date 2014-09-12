25f70079c6b5e6d40244d9da1877f763562b70d4
Mike Slinn <mslinn@micronauticsresearch.com>
2014-09-12 15:09:26 +0100

    Works

--- src/main/scala/VTest.scala
+++ src/main/scala/VTest.scala
@@ -2,6 +2,14 @@ import java.io.File
 import org.apache.commons.vfs2._
 
 object VTest extends App {
+  val bucket = {
+    if (args.length!=1) {
+      println("Usage: VTest bucketName")
+      System.exit(-2)
+    }
+    args(0)
+  }
+
   val fsManager = {
     import java.io.FileInputStream
     import java.util.Properties
@@ -27,7 +35,7 @@ object VTest extends App {
   dir.createFolder()
 
   // Upload file to S3
-  val dest: FileObject = fsManager.resolveFile("s3://vfs-test/README.md")
+  val dest: FileObject = fsManager.resolveFile(s"s3://$bucket/README.md")
   val src: FileObject = fsManager.resolveFile(new File("README.md").getAbsolutePath)
   dest.copyFrom(src, Selectors.SELECT_SELF)
 }
